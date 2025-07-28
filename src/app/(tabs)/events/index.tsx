9c9e4f94e51422d3d37744b3b689a86161d28dc6
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-28 11:14:43 +0100

    Create index.tsx

---
+++ src/app/(tabs)/events/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function EventsScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Events Screen</Text>
+    </View>
+  );
+}
