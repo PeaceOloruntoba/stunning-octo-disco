e0d6c7533b8e0f21b1f3ab3dc9081770c29f2554
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-28 09:45:31 +0100

    Update signup.tsx

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -7,10 +7,13 @@ import Checkbox from "expo-checkbox";
 export default function SignupScreen() {
   const [lastName, setLastName] = useState("");
   const [firstName, setFirstName] = useState("");
+  const [alter, setAlter] = useState("");
+  const [geschlecht, setGeschlecht] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [newsletter, setNewsletter] = useState(false);
+  const [datenschutz, setDatenschutz] = useState(false);
   const { signup, error } = useSignup();
   const router = useRouter();
 
@@ -21,24 +24,38 @@ export default function SignupScreen() {
   return (
     <View className="flex-1 bg-gray-100 justify-center items-center p-5">
       <Text className="text-2xl font-bold mb-5">Registriere dich!</Text>
-      <TextInput
-        className="w-1/2 p-3 mb-4 bg-white rounded-lg"
-        placeholder="Nachname"
-        value={lastName}
-        onChangeText={setLastName}
-      />
-      <TextInput
-        className="w-1/2 p-3 mb-4 bg-white rounded-lg"
-        placeholder="Vorname"
-        value={firstName}
-        onChangeText={setFirstName}
-      />
+      <View className="flex-row w-full mb-4 gap-4">
+        <TextInput
+          className="flex-1 p-3 bg-white rounded-lg"
+          placeholder="Nachname"
+          value={lastName}
+          onChangeText={setLastName}
+        />
+        <TextInput
+          className="flex-1 p-3 bg-white rounded-lg"
+          placeholder="Vorname"
+          value={firstName}
+          onChangeText={setFirstName}
+        />
+      </View>
       <TextInput
         className="w-full p-3 mb-4 bg-white rounded-lg"
         placeholder="E-Mail"
         value={email}
         onChangeText={setEmail}
         autoCapitalize="none"
+      />
+      <TextInput
+        className="w-full p-3 mb-4 bg-white rounded-lg"
+        placeholder="Alter"
+        value={alter}
+        onChangeText={setAlter}
+      />
+      <TextInput
+        className="w-full p-3 mb-4 bg-white rounded-lg"
+        placeholder="Geschlecht"
+        value={geschlecht}
+        onChangeText={setGeschlecht}
       />
       <TextInput
         className="w-full p-3 mb-4 bg-white rounded-lg"
@@ -59,23 +76,28 @@ export default function SignupScreen() {
           <Checkbox
             value={newsletter}
             onValueChange={setNewsletter}
-            color={newsletter ? "#6b46c1" : undefined}
+            color={newsletter ? "#3B82F6" : undefined}
           />
           <View className="ml-2">
             <Text>Newsletter</Text>
-            <Text>Daten Schutz blab bla bla</Text>
+            <Text>Checkbox</Text>
           </View>
         </View>
-        <View className="flex-1 p-3 bg-white rounded-lg flex-row items-center">
-          <Checkbox value={false} color={"#a0aec0"} />
+        <View className="flex-1 p-3 bg-white rounded-lg mr-2 flex-row items-center">
+          <Checkbox
+            value={datenschutz}
+            onValueChange={setDatenschutz}
+            color={datenschutz ? "#3B82F6" : undefined}
+          />
           <View className="ml-2">
-            <Text>Daten Schutz blab bla bla</Text>
+            <Text>Datenschutz</Text>
+            <Text>Checkbox</Text>
           </View>
         </View>
       </View>
       {error && <Text className="text-red-500 mb-5">{error}</Text>}
       <TouchableOpacity
-        className="w-full p-3 bg-purple-500 rounded-lg"
+        className="w-full p-3 bg-[#3B82F6] rounded-lg"
         onPress={handleSignup}
       >
         <Text className="text-white text-center">Jetzt registrieren</Text>
@@ -84,7 +106,7 @@ export default function SignupScreen() {
         className="mt-5"
         onPress={() => router.push("/(auth)/login")}
       >
-        <Text className="text-purple-500">
+        <Text className="text-[#3B82F6]">
           Bereits ein Account? Jetzt einloggen
         </Text>
       </TouchableOpacity>
