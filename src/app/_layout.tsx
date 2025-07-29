c3e01cb44a7dce53e5c6893295ad5986355cbdde
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-29 18:43:15 +0100

    Update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,28 @@
 import "../global.css";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { useAuthState } from "../hooks/auth";
 
 export default function RootLayout() {
+  const router = useRouter();
+  const { user, loading } = useAuthState();
+
+  useEffect(() => {
+    if (!loading) {
+      if (user) {
+        router.replace("/(auth)/check-profile");
+      } else {
+        router.replace("/(auth)/login");
+      }
+    }
+  }, [user, loading, router]);
+
   return (
     <SafeAreaProvider>
       <StatusBar style="dark" />
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="preferences" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
